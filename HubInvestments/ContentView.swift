2271f96bfeda3591972add91ddd40bcf033d744f
Yan Rodrigues <yan.rodrigues@avenue.us>
2024-11-21 21:12:23 -0300

    Initial Commit

---
+++ HubInvestments/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HubInvestments
+//
+//  Created by Yan Rodrigues on 21/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HubInvestments/HubInvestmentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HubInvestmentsApp.swift
+//  HubInvestments
+//
+//  Created by Yan Rodrigues on 21/11/24.
+//
+
+import SwiftUI
+
+@main
+struct HubInvestmentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
